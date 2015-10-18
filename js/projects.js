50fea8c2f8423243f0c64aceb4017487ecadb0f5
Konstantin A. Zolotukhin <masted@gmail.com>
2015-10-19 02:46:23 +0300

    Create projects.js

---
+++ js/projects.js
@@ -0,0 +1,18 @@
+oss_projects['ametiste-metrics'] = {
+  featured: true,
+  position: 1
+  // background: 'elastalert.png'
+}
+
+oss_projects['ametiste-scm'] = {
+  featured: true,
+  position: 2
+  // background: 'elastalert.png'
+}
+
+oss_projects['redgreen'] = {
+  featured: true,
+  position: 3
+  // background: 'elastalert.png'
+}
+
