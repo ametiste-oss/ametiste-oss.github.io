72de6c947da2275b75f9e26693b26c1d7fb81bc9
Konstantin A. Zolotukhin <masted@gmail.com>
2015-10-19 03:02:12 +0300

    Update projects.js

--- js/projects.js
+++ js/projects.js
@@ -1,19 +1,19 @@
 oss_projects = {};
 oss_projects['ametiste-metrics'] = {
   featured: true,
-  position: 1
-  // background: 'elastalert.png'
+  position: 1,
+  background: 'project-stub.png'
 }
 
 oss_projects['ametiste-scm'] = {
   featured: true,
-  position: 2
-  // background: 'elastalert.png'
+  position: 2,
+  background: 'project-stub.png'
 }
 
 oss_projects['redgreen'] = {
   featured: true,
-  position: 3
-  // background: 'elastalert.png'
+  position: 3,
+  background: 'project-stub.png'
 }
 
